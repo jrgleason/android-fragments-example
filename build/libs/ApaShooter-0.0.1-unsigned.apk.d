59d59f4cfc6f0d9aa7b090654c1763d08668e60f
jackie <jackie@jackie-Latitude-E6410.(none)>
2012-02-29 16:54:57 -0500

    Removing local files

--- build/libs/ApaShooter-0.0.1-unsigned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/jackie/Development/Code/personal/Android/ApaShooter/build/libs/ApaShooter-0.0.1-unsigned.apk : \
-/home/jackie/Development/Code/personal/Android/ApaShooter/build/libs/ApaShooter.ap_ \
-/home/jackie/Development/Code/personal/Android/ApaShooter/build/libs/classes.dex \
