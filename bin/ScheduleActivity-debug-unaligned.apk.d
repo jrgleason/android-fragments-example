771f4a55bb3dbebb1024947c73493670aa02833f
jackie <jackie@jackie-Latitude-E6410.(none)>
2012-03-13 10:00:11 -0400

    Some updates

--- bin/ScheduleActivity-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/home/jackie/Development/Code/personal/Android/ApaShooter/bin/ScheduleActivity-debug-unaligned.apk : \
-/home/jackie/Development/Code/personal/Android/ApaShooter/bin/ScheduleActivity.ap_ \
-/home/jackie/Development/Code/personal/Android/ApaShooter/bin/classes.dex \
-/home/jackie/Development/Code/personal/Android/ApaShooter/src/com/gleason/apa/model/AndroidCalendar.java.bkp \
-/home/jackie/Development/Code/personal/Android/ApaShooter/src/com/gleason/apa/fragment/ScheduleFragment.java.bkp \
-/home/jackie/Development/Code/personal/Android/ApaShooter/libs/android-support-v4.jar \
--- bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/home/jackie/Development/Code/personal/Android/ApaShooter/bin/classes.dex : \
-/home/jackie/Development/Code/personal/Android/ApaShooter/libs/android-support-v4.jar \
